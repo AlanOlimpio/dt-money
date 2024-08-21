bb371c7c0e02a5b8d276c478893a9dd80d0e4e73
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-08-21 15:25:34 -0300

    feat: cria tabela de transações

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,39 @@
 import Header from '../../components/Header';
 import Summary from '../../components/Summary';
+import {
+  ListTd,
+  PrinceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './TransactionsStyles';
 
 function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <ListTd width="50%">Desenvolvimento de site</ListTd>
+              <ListTd>
+                <PrinceHighLight variant="income">R$ 12.000,00</PrinceHighLight>
+              </ListTd>
+              <ListTd>Venda</ListTd>
+              <ListTd>13/04/2022</ListTd>
+            </tr>
+            <tr>
+              <ListTd width="50%">Hamburguer</ListTd>
+              <ListTd>
+                <PrinceHighLight variant="outcome">- R$ 59,00</PrinceHighLight>
+              </ListTd>
+              <ListTd>Alimentação</ListTd>
+              <ListTd>10/04/2022</ListTd>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
---
+++ src/pages/Transactions/TransactionsStyles.ts
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components';
+
+interface listProps {
+  width?: '10%' | '20%' | '30%' | '40%' | '50%';
+}
+
+interface PrinceHighLightProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionsContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 5rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+`;
+
+export const ListTd = styled.td<listProps>`
+  ${({ theme, width }) => css`
+    ${width &&
+    css`
+      width: ${width};
+    `}
+    padding: 1.25rem 2rem;
+    background-color: ${theme['gray-700']};
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  `}
+`;
+
+export const PrinceHighLight = styled.span<PrinceHighLightProps>`
+  ${({ theme, variant }) => css`
+    color: ${variant === 'income' ? theme['green-300'] : theme['red-300']};
+  `}
+`;
